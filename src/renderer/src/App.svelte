a7272dacfadd1896c19d9eae05a0630dc285e553
Jerry Weiler <jweiler64@hotmail.com>
2025-03-25 00:47:26 -0700

    apply prettier cleanup

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import Versions from './components/Versions.svelte'
-  import electronLogo from './assets/electron.svg'
+  import Versions from "./components/Versions.svelte";
+  import electronLogo from "./assets/electron.svg";
 
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
 </script>
 
 <img alt="logo" class="logo" src={electronLogo} />
@@ -16,7 +16,9 @@
 <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
 <div class="actions">
   <div class="action">
-    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+      Documentation
+    </a>
   </div>
   <div class="action">
     <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
